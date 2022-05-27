3dafa4fbb75886636e62d10e9e995a414534fc47
mohammad <mohammad.meras.1377@gmail.com>
2022-05-27 16:28:13 +0430

    git

--- src/layouts/admin/Index.jsx
+++ src/layouts/admin/Index.jsx
@@ -2,8 +2,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { useContext } from "react";
 import { AdminContext, AdminContextContainer } from "../../context/adminLayoutContext";
+import Category from "../../pages/Category/Category";
 import Dashboard from "../../pages/dashboard/Dashboard";
-import AddProduct from "../../pages/Product/AddProduct";
 import Product from "../../pages/Product/Product";
 
 import Header from "./header/index";
@@ -17,7 +17,8 @@ const Index = () => {
             <section id="content_section" className={`bg-light py-2 px-3 ${showSlider ? "with_sidebar": null}`}>
                 
                 {/* <Dashboard/> */}
-                <Product/>
+                {/* <Product/> */}
+                <Category/>
             </section>
             
             
